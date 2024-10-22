6c68384922413eb30a13cefdb5d190a9326ba678
Dmitry Slobodchikov <zoosmand@gmail.com>
2024-10-22 13:28:55 +0300

    Configure lintnig the code

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,33 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default [
-  {languageOptions: { globals: globals.browser }},
+let rules = {};
+
+rules.libs = [
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    },
+    ignores: [
+      "lib/data.js"
+    ]
+  }
 ];
+
+
+export default rules.libs;
